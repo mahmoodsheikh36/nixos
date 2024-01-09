958cb1a66087e1689878bb97d0a0f4245f66a33d
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-01-09 13:42:26 +0200

    disable wireless (bare wpa) to make it work

--- myimage.nix
+++ myimage.nix
@@ -1,9 +1,16 @@
 { pkgs, modulesPath, lib, ... }: {
   imports = [
-    # "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    # provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    # <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
     "/etc/nixos/configuration.nix"
   ];
+  # recommended by the wiki for speeding up build time
+  # suboptimal for production tho :p
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+  networking.wireless.enable = false; # installation-cd-minimal.nix sets that to true
 
-  # Needed for https://github.com/NixOS/nixpkgs/issues/58959
+  # needed for https://github.com/nixos/nixpkgs/issues/58959
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 }
