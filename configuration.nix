0ecf0e946de7f413ba7c686fa7af17725e04820e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-02-12 22:02:22 +0200

    add some env vars, refactor

--- configuration.nix
+++ configuration.nix
@@ -223,8 +223,8 @@
     # block some hosts by redirecting to the loopback interface
     extraHosts = ''
         192.168.1.150 server
-        # 127.0.0.1 youtube.com
-        # 127.0.0.1 www.youtube.com
+        127.0.0.1 youtube.com
+        127.0.0.1 www.youtube.com
         # 127.0.0.1 reddit.com
         # 127.0.0.1 www.reddit.com
         127.0.0.1 discord.com
@@ -534,17 +534,7 @@
     # enchant.dev # for emacs jinx-mode
 
     (emacsWithPackagesFromUsePackage {
-      # Your Emacs config file. Org mode babel files are also
-      # supported.
-      # NB: Config files cannot contain unicode characters, since
-      #     they're being parsed in nix, which lacks unicode
-      #     support.
-      # config = ./emacs.org;
       config = "/dev/null";
-
-      # Whether to include your config as a default init file.
-      # If being bool, the value of config is used.
-      # Its value can also be a derivation like this if you want to do some
       # substitution:
       #   defaultInitFile = pkgs.substituteAll {
       #     name = "default.el";
@@ -552,34 +542,12 @@
       #     inherit (config.xdg) configHome dataHome;
       #   };
       defaultInitFile = false;
-
-      # Package is optional, defaults to pkgs.emacs
       package = emacs-git;
-
-      # By default emacsWithPackagesFromUsePackage will only pull in
-      # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
-      # Setting `alwaysEnsure` to `true` emulates `use-package-always-ensure`
-      # and pulls in all use-package references not explicitly disabled via
-      # `:ensure nil` or `:disabled`.
-      # Note that this is NOT recommended unless you've actually set
-      # `use-package-always-ensure` to `t` in your config.
       alwaysEnsure = true;
-
-      # For Org mode babel files, by default only code blocks with
-      # `:tangle yes` are considered. Setting `alwaysTangle` to `true`
-      # will include all code blocks missing the `:tangle` argument,
-      # defaulting it to `yes`.
-      # Note that this is NOT recommended unless you have something like
-      # `#+PROPERTY: header-args:emacs-lisp :tangle yes` in your config,
-      # which defaults `:tangle` to `yes`.
-      # alwaysTangle = true;
-
-      # Optionally provide extra packages not in the configuration file.
       extraEmacsPackages = epkgs: [
         epkgs.cask
         # epkgs.jinx
       ];
-
       # Optionally override derivations.
       # override = final: prev: {
       #   weechat = prev.melpaPackages.weechat.overrideAttrs(old: {
@@ -633,6 +601,8 @@
     # this one fixes some problems with python matplotlib and probably some other qt applications
     QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
     PYTHON_HISTORY_FILE = "$HOME/brain/python_history";
+    BRAIN_DIR = "$HOME/brain";
+    SCRIPTS_DIR = "$HOME/work/scripts/";
     QT_SCALE_FACTOR = "2";
     EDITOR = "nvim";
   };
