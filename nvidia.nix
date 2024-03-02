800baad8e158296bfb0b3ccdc05d216039dcd1f7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-02 12:55:00 +0200

    configure nvidia drivers

--- nvidia.nix
+++ nvidia.nix
@@ -11,7 +11,7 @@
         enable = true;
         enableOffloadCmd = true;
       };
-      sync.enable = true;
+      # sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
