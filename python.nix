ccd4e877ecc3b26995e4e3a8f465286dabfb5888
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-03 15:01:59 +0200

    python dev shell file

---
+++ python.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (ps: with ps; [
+      matplotlib flask requests panflute numpy jupyter jupyter-core pytorch pandas sympy scipy
+      scikit-learn torchvision scrapy beautifulsoup4 seaborn pillow dash mysql-connector
+      rich pyspark networkx dpkt python-lsp-server #opencv
+      graphviz
+    ]))
+  ];
+}
