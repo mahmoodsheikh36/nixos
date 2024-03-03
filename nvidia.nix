bc1784ef1ab24bc0d625960cdd0f50d7919f6114
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-03 14:21:25 +0200

    configure nvidia drivers

--- nvidia.nix
+++ nvidia.nix
@@ -4,18 +4,18 @@
   hardware.nvidia = {
     modesetting.enable = true;
     nvidiaSettings = true;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       # make sure to use the correct bus id values for your system!
       # offload = {
       #   enable = true;
       #   enableOffloadCmd = true;
       # };
-      # sync.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+      sync.enable = true;
+      intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:01:00:0";
     };
   };
-  boot.kernelParams = [ "module_blacklist=i915" ];
+  # boot.kernelParams = [ "module_blacklist=i915" ];
   boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 }
