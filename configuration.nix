ce96fb1b5742c20bbf51f7b58c3b15e2326457d5
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-04-30 23:53:29 +0300

    disable tumbler

--- configuration.nix
+++ configuration.nix
@@ -331,6 +331,7 @@
   programs.wireshark.enable = true;
   programs.dconf.enable = true;
   # programs.firefox.enable = true;
+  services.tumbler.enable = false;
 
   services.mysql = {
     enable = true;
