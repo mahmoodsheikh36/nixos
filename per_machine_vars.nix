5cc4ffee9e3653c5294e7f81a4e8b59c855364f4
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-26 11:29:06 +0300

    add comment on variable

--- per_machine_vars.nix
+++ per_machine_vars.nix
@@ -1,4 +1,4 @@
 # variables that need to be set per machine
 {}: {
-  remote_tunnel_port = 5000;
+  remote_tunnel_port = 5000; # the port to open for the machine on the main server (for tunneling)
 }
