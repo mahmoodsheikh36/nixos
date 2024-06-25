dd17f7274937d894b271f6cc868dde3deafb0f8e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-06-26 02:25:12 +0300

    change emacs overlay sha256

--- server_vars.nix
+++ server_vars.nix
@@ -3,7 +3,7 @@
   server_overlays = [
     (import (builtins.fetchTarball { # emacs master
       url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-      sha256 = "00wfi5mm31ciliydl50h329ia1d1rxmf52qyl2z9w9in3wfqmb12"; # to avoid an error
+      sha256 = "0hh1vwy4dlxq8mj4vh1q1346y3n10zimj53ifh83j4s9mcc578ja"; # to avoid an error
     }))
     # (self: super:
     #   {
