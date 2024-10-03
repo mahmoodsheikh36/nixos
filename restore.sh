5b3682fa5308c7e3158f482a63938a17c265eb03
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-03 18:18:42 +0300

    have to regenerate hardware-configuration on every machine

--- restore.sh
+++ restore.sh
@@ -2,3 +2,4 @@ sudo ln -sf "$PWD"/flake.nix /etc/nixos
 sudo cp /etc/nixos/hardware-configuration.nix "$PWD/"
 sudo mv /etc/nixos/ /tmp/moved_nixos
 sudo ln -sf "$PWD"/ /etc/nixos
+sudo nixos-generate-config
